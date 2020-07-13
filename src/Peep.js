0466242b28bfb288613620134037c37734ff83cf
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-07-13 12:50:30 +0100

    Add moment to display date correctly

--- src/Peep.js
+++ src/Peep.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
+import 'moment-timezone';
 
 class Peep extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    console.log(this.props.post)
-    const { body, date, id, likes, updated, user } = this.props.post;
+    const { id, body, created_at, likes, updated, user } = this.props.post;
     return (
       <div>
         <p>{body}</p>
-        <p>Posted by {user.handle}</p>
+        <p>Posted by {user.handle} <Moment fromNow tz="Europe/London">{created_at}</Moment></p>
         { this.props.post.likes.length > 0 ? (
           <p>{this.props.post.likes.length}</p>
         ) : ('')}
-
       </div>
     )
   }
